da783091a95fc77513f92c5552e6010c2702ea74
Treycos <maxime.poitevin@protonmail.com>
2023-12-16 12:53:33 +0100

    Hash location test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { AppShell, Burger, NavLink, ScrollArea } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import { Link, Redirect, Route, Switch } from "wouter";
+import { Link, Redirect, Route, Router, Switch } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 
 import Logo from "./assets/chef.svg?react";
 import { RecipeGrid } from "./components/RecipeGrid";
@@ -14,50 +15,62 @@ function App() {
   const [opened, { toggle }] = useDisclosure();
 
   return (
-    <AppShell
-      header={{ height: 60 }}
-      navbar={{
-        width: 300,
-        breakpoint: "sm",
-        collapsed: { mobile: !opened },
-      }}
-      padding="md"
-    >
-      <AppShell.Header className={styles.header}>
-        <div className={styles.topLeft}>
-          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-          <Logo className={styles.logo} />
-        </div>
-        <SearchRecipe />
-      </AppShell.Header>
-      <AppShell.Navbar p="md">
-        <ScrollArea>
-          {Object.entries(ImportedRecipes).map(([type, recipeGroup]) => (
-            <NavLink component={Link} href={`/${type}`} key={type} label={type}>
-              {Object.entries(recipeGroup).map(([slug, { title }]) => (
-                <NavLink
-                  component={Link}
-                  href={`/${type}/${slug}`}
-                  key={slug}
-                  label={title}
-                />
-              ))}
-            </NavLink>
-          ))}
-        </ScrollArea>
-      </AppShell.Navbar>
-      <AppShell.Main className={styles.main}>
-        <Switch>
-          <Route path="/:type">
-            <RecipeGrid />
-          </Route>
-          <Route path="/:type/:recipe">
-            <RecipePage />
-          </Route>
-          <Redirect to="/" />
-        </Switch>
-      </AppShell.Main>
-    </AppShell>
+    <Router hook={useHashLocation}>
+      <AppShell
+        header={{ height: 60 }}
+        navbar={{
+          width: 300,
+          breakpoint: "sm",
+          collapsed: { mobile: !opened },
+        }}
+        padding="md"
+      >
+        <AppShell.Header className={styles.header}>
+          <div className={styles.topLeft}>
+            <Burger
+              opened={opened}
+              onClick={toggle}
+              hiddenFrom="sm"
+              size="sm"
+            />
+            <Logo className={styles.logo} />
+          </div>
+          <SearchRecipe />
+        </AppShell.Header>
+        <AppShell.Navbar p="md">
+          <ScrollArea>
+            {Object.entries(ImportedRecipes).map(([type, recipeGroup]) => (
+              <NavLink
+                component={Link}
+                href={`/${type}`}
+                key={type}
+                label={type}
+              >
+                {Object.entries(recipeGroup).map(([slug, { title }]) => (
+                  <NavLink
+                    component={Link}
+                    href={`/${type}/${slug}`}
+                    key={slug}
+                    label={title}
+                  />
+                ))}
+              </NavLink>
+            ))}
+          </ScrollArea>
+        </AppShell.Navbar>
+        <AppShell.Main className={styles.main}>
+          <Switch>
+            <Route path="/:type">
+              <RecipeGrid />
+            </Route>
+            <Route path="/:type/:recipe">
+              <RecipePage />
+            </Route>
+            <Redirect to="/" />
+          </Switch>
+        </AppShell.Main>
+      </AppShell>
+    </Router>
   );
 }
 
